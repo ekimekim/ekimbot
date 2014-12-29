e0eb1b548ff0718fd7a0319d9c10cd610191bfba
Mike Lang <mikelang3000@gmail.com>
2014-12-29 09:10:27 -0800

    Fixes for config manager

--- ekimbot/core_plugins/config_manager.py
+++ ekimbot/core_plugins/config_manager.py
@@ -1,4 +1,7 @@
 
+import logging
+import ast
+
 from ekimbot.botplugin import BotPlugin, CommandHandler
 from ekimbot.config import config
 
@@ -9,11 +12,14 @@ class ConfigManagerPlugin(BotPlugin):
 	@CommandHandler('config load', 1)
 	def load_config(self, msg, *args):
 		path = ' '.join(args)
+		self.logger.debug("Loading config from {!r}".format(path))
 		try:
 			config.from_file(path)
 		except Exception as ex:
 			self.reply(msg, "Failure to load config file {!r} with {}: {}".format(path, type(ex).__name__, ex))
 		else:
+			self.refresh()
+			self.logger.info("Loaded config from {!r}".format(path))
 			self.reply(msg, "Config file {!r} loaded".format(path))
 
 	@CommandHandler('config set', 2)
@@ -21,7 +27,14 @@ class ConfigManagerPlugin(BotPlugin):
 		value = ' '.join(value)
 		try:
 			value = ast.literal_eval(value)
-		except SyntaxError:
+		except Exception:
 			pass
+		self.logger.debug("Setting config.{} = {!r}".format(key, value))
 		config[key] = value
+		self.refresh()
+		self.logger.info("Set config.{} = {!r}".format(key, value))
 		self.reply(msg, 'set config.{} = {!r}'.format(key, value))
+
+	def refresh(self):
+		logging.getLogger().setLevel(logging._levelNames[config.loglevel.upper()]
+		                             if isinstance(config.loglevel, basestring) else config.loglevel)
